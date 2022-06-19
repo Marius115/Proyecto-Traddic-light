f2829ec37096defbad8ef3eba1790cfc7871e494
Marius115 <cascaranomeister@gmail.com>
2022-06-19 04:18:42 +0000

    Traffic light v2.Agregado el useState

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
 //create your first component
 const Home = () => {
+	const [selectedColor, setselectedColor ] = useState("red");
+
 	return (
 		<div className="Traffic-light">
-			<div className="light red glow mb-1"></div>
-			<div className="light yellow mb-1"></div>
-			<div className="light green mt-1"></div>
+			<div className={"light red mb-2"+((selectedColor === "red") ? " glow" : "")}></div>
+			<div className={"light yellow mb-1"+((selectedColor === "yellow") ? " glow" : "")}></div>
+			<div className={"light green mt-1"+((selectedColor === "green") ? " glow" : "")}></div>
 			
 		</div>
 	);
